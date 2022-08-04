6942220531ca293b6b042af9d92ea41cba3217cc
Your Name <mustafa6143170@gmail.com>
2022-08-05 00:43:09 +0500

    push

--- 06-Tavakkal-Game/starter/script.js
+++ 06-Tavakkal-Game/starter/script.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 //const totalScore0 = document.querySelector('#score--0');
-const totalScore0 = document.getElementById('score--0');
-const totalScore1 = document.getElementById('score--1');
-const currentScore0 = document.getElementById('current--0');
-const currentScore1 = document.getElementById('current--1');
-const dice = document.querySelector('.dice');
-const btnRoll = document.querySelector('.btn--roll');
-const player0 = document.querySelector('.player--0');
-const player1 = document.querySelector('.player--1');
-const holdBtn = document.querySelector('.btn--hold');
-const newBtn = document.querySelector('.btn--new');
+const totalScore0 = document.getElementById("score--0");
+const totalScore1 = document.getElementById("score--1");
+let currentScore0 = document.getElementById("current--0");
+let currentScore1 = document.getElementById("current--1");
+const dice = document.querySelector(".dice");
+const btnRoll = document.querySelector(".btn--roll");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
+const holdBtn = document.querySelector(".btn--hold");
+const newBtn = document.querySelector(".btn--new");
 
 let currentScore,
   activeUser,
@@ -19,37 +19,40 @@ let currentScore,
 const init = () => {
   totalScore0.textContent = 0;
   totalScore1.textContent = 0;
+  currentScore0.textContent = 0;
+  currentScore1.textContent = 0;
   currentScore = 0;
+  console.log(currentScore);
   activeUser = 0;
   totalScores = [0, 0];
 
-  player0.classList.remove('player--winner');
-  player1.classList.remove('player--winner');
+  player0.classList.remove("player--winner");
+  player1.classList.remove("player--winner");
 
-  btnRoll.removeAttribute('disabled');
-  btnRoll.style.cursor = 'pointer';
-  holdBtn.removeAttribute('disabled');
-  holdBtn.style.cursor = 'pointer';
+  btnRoll.removeAttribute("disabled");
+  btnRoll.style.cursor = "pointer";
+  holdBtn.removeAttribute("disabled");
+  holdBtn.style.cursor = "pointer";
 
-  player0.classList.add('player--active');
-  player1.classList.remove('player--active');
+  player0.classList.add("player--active");
+  player1.classList.remove("player--active");
 };
 
 init();
 
-const showCurrentScore = activeId => {
-  document.getElementById('current--' + activeId).textContent = currentScore;
+const showCurrentScore = (activeId) => {
+  document.getElementById("current--" + activeId).textContent = currentScore;
 };
 
 const changeActiveClass = () => {
-  player0.classList.toggle('player--active');
-  player1.classList.toggle('player--active');
+  player0.classList.toggle("player--active");
+  player1.classList.toggle("player--active");
 };
 
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   let randomNumber = Math.trunc(Math.random() * 6) + 1;
-  dice.classList.remove('hidden');
-  dice.src = 'img/dice-' + randomNumber + '.png';
+  dice.classList.remove("hidden");
+  dice.src = "img/dice-" + randomNumber + ".png";
 
   if (randomNumber !== 1) {
     currentScore += randomNumber;
@@ -63,27 +66,27 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
-holdBtn.addEventListener('click', function () {
-  dice.classList.add('hidden');
+holdBtn.addEventListener("click", function () {
+  dice.classList.add("hidden");
 
   totalScores[activeUser] += currentScore;
   currentScore = 0;
 
   showCurrentScore(activeUser);
-  document.getElementById('score--' + activeUser).textContent =
+  document.getElementById("score--" + activeUser).textContent =
     totalScores[activeUser];
   if (totalScores[activeUser] >= 100) {
     document
-      .querySelector('.player--' + activeUser)
-      .classList.add('player--winner');
-    btnRoll.setAttribute('disabled', true);
-    btnRoll.style.cursor = 'not-allowed';
-    holdBtn.setAttribute('disabled', true);
-    holdBtn.style.cursor = 'not-allowed';
+      .querySelector(".player--" + activeUser)
+      .classList.add("player--winner");
+    btnRoll.setAttribute("disabled", true);
+    btnRoll.style.cursor = "not-allowed";
+    holdBtn.setAttribute("disabled", true);
+    holdBtn.style.cursor = "not-allowed";
   }
 
   activeUser = activeUser === 0 ? 1 : 0;
   changeActiveClass();
 });
 
-newBtn.addEventListener('click', init);
+newBtn.addEventListener("click", init);
