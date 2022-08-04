1e11c971a8f0458e803b83aefeb023bca9689e55
Your Name <mustafa6143170@gmail.com>
2022-08-04 21:26:37 +0500

    Revert "Revert "push""
    This reverts commit 53805cad7ed78d84447b7c6dd28e41cc8034b932.

---
+++ project_empty/src/components/CreateNft/CreateNft.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./CreateNft.scss";
+import esco from "../.././assets/img/esco.png";
+const CreateNft = () => {
+  return (
+    <>
+      <div className="createNft mb-5">
+        <div className="footer-line"></div>
+
+        <div className="row">
+          <div className="col-xl-6 col-sm-12">
+            <p className="createNft_p">Save your time with Stacks</p>
+            <h1>Earn free crypto with Crypter</h1>
+            <p className="mb-5 mt-3">A creative agency that lead and inspire</p>
+            <div className="createNft_btns ">
+              <button className="btn btn-primary me-3 btns "> Learn now</button>
+              <button className="btn  btn-light btns"> Discover more</button>
+            </div>
+          </div>
+          <div className="col-xl-6 col-sm-12">
+            <img className="esco w-100 d-block" src={esco} alt="esco" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreateNft;
---
+++ project_empty/src/components/SectionCarousel/Persons/Persons.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Persons.scss";
+import plus from "../../../assets/icons/plus.svg";
+import top from "../../../assets/icons/up.svg";
+import heart from "../../../assets/icons/heart.svg";
+export const Persons = (props) => {
+  const { images, badges, names } = props;
+
+  return (
+    <div className="card">
+      <div className="card-head">
+        <div className="achieved  d-flex">
+          <img className="achivied_img" src={badges} alt="achieved" />
+          <div>
+            <img className="plus" src={plus} alt="plus" />
+            <span>
+              <img className="top" src={top} alt="top" />
+            </span>
+          </div>
+        </div>
+        <div className="card-line"></div>
+        <div className="card-body">
+          <img className="person_img" src={images} alt="person" />
+          <span>
+            <img className="heart" src={heart} alt="diamond" />
+          </span>
+          <div className="person_name">{names}</div>
+          <div className="item_price">
+            2.456 <span>ETH</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
