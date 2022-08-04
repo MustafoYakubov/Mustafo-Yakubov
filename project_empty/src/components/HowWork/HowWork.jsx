53805cad7ed78d84447b7c6dd28e41cc8034b932
Your Name <mustafa6143170@gmail.com>
2022-08-04 21:20:47 +0500

    Revert "push"
    This reverts commit becc3c672a983fbf1cd212a5054e59e99b6a7944.

--- project_empty/src/components/HowWork/HowWork.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function HowWork() {
-  return (
-    <div className='container'>HowWork</div>
-  )
-}
--- project_empty/src/components/Notification/Notification.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Notification() {
-  return <div className="container">Notification</div>;
-}
