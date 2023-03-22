21f4083bb4649d40fbc1fff2d065d59ff23a8489
Mustafo Yakubov <mustafa6143170@gmail.com>
2023-03-23 02:15:29 +0500

    push

--- Tesla/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import { AuthUserProvider } from '../context/AuthUserContext.js'
-
-function MyApp({ Component, pageProps }) {
-  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>
-}
-
-export default MyApp;
